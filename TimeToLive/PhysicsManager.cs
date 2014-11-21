9f0defd8aa9b31df6e79094f07c9984db321d3b9
louisMyu <louisMyu@gmail.com>
2014-11-21 06:50:46 -0800

    adding animation manager class, forgot to add in previous commit

---
+++ TimeToLive/PhysicsManager.cs
@@ -0,0 +1,68 @@
+﻿using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeToLive
+{
+    public class PhysicsManager
+    {
+        private Stack<Body> m_PhysicsBodies;
+        private const int MAX_NUM_BODIES = 500;
+        private World m_World;
+        public PhysicsManager()
+        {
+            m_PhysicsBodies = new Stack<Body>();
+            m_World = new World(new Vector2(0,0));
+        }
+
+        public void Init()
+        {
+            for (int i = 0; i < MAX_NUM_BODIES; ++i)
+            {
+                Body temp = BodyFactory.CreateBody(m_World);
+                temp.Enabled = false;
+                m_PhysicsBodies.Push(temp);
+            }
+        }
+        public void Update(float dt)
+        {
+            m_World.Step(dt);
+        }
+        public Body GetBody(float radius, float density, Vector2 position, out Fixture attached)
+        {
+            Body bodyFromPool;
+            if (m_PhysicsBodies.Count != 0)
+            {
+                bodyFromPool = m_PhysicsBodies.Pop();
+            }
+            else
+            {
+                bodyFromPool = BodyFactory.CreateBody(m_World);
+            }
+            bodyFromPool.Position = position;
+            attached = FixtureFactory.AttachCircle(radius, density, bodyFromPool);
+            bodyFromPool.Enabled = true;
+            return bodyFromPool;
+        }
+
+        public void ReturnBody(Body body)
+        {
+            if (m_PhysicsBodies.Count >= MAX_NUM_BODIES)
+            {
+                m_World.RemoveBody(body);
+                body.Dispose();
+                return;
+            }
+            body.Enabled = false;
+            
+            body.DestroyFixture(body.FixtureList[0]);
+
+            m_PhysicsBodies.Push(body);
+        }
+    }
+}
